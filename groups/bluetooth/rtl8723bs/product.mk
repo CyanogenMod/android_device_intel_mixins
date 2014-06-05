410668b55769e39ed2e98423fd8bfdd6913ce8e0
Jianxun Zhang <jianxun.zhang@intel.com>
2014-06-04 17:58:59 -0700

    Bluetooth: Add RT8723BS group and refactor bcm43241 (ASUS T100T)
    Change-Id: I84014d867c5db70fcc345c419e40409ab5f711ad
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>

---
+++ groups/bluetooth/rtl8723bs/product.mk
@@ -0,0 +1,9 @@
+PRODUCT_PACKAGES += \
+    hciattach \
+    audio.a2dp.default \
+    rtl8723b_config \
+    rtl8723b_fw \
+
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
