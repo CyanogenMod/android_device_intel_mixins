2bc182b04a14378c3c224f6b7cd3ab00601b6a61
Andy Ross <andrew.j.ross@intel.com>
2014-06-06 10:52:26 -0700

    camera: merge topic changes into mixin targets
    Port the following camera branch changes (originally in
    device/intel/gmin) to the mixin strategy for asus_t100 and byt_mrd7.
    
    Change-Id: Ifbe91693835827c9dd4ca315ee75e73df76f6abb
        commit f857b828db198f1e9e5109e6b3543a249221415a
        Author: Andy Ross <andrew.j.ross@intel.com>
        Date:   Fri May 30 13:46:26 2014 -0700
    
            Fix camera module loading
    
            The atomisp driver suite is order-sensitive, so blacklist it in
            libkmod configuration to prevent hald loading the modules.  Then load
            them in order in a service when hald is finished.
    
            Note that it can't be done synchronously: atomisp requests firmware
            under its module init function, and the uevent for the request won't
            be serviced by init until the insmod call returns!
    
            Change-Id: I8ef059fcc05cf392826124dfde3b7adc17dd51fb
            Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
        commit 1d1c5fe295123384780c6917b19f1658854fbb79
        Author: Mikael Persson <mikael.persson@intel.com>
        Date:   Fri May 23 13:06:30 2014 +0200
    
            Camera: update config flags.
    
            Remove unused config flags and disable
            unused functionality: IntelCamera, live face video effect,
            and libmix image decoder.
            Add libmix_videovpp to PRODUCT_PACKAGES.
    
            Change-Id: I0d9f088cdfae6e612e8accf1b56968843cd896dd
            Depends-Change-Id: I575d10fcbba0e95327eba94c8097990c2785516b
            Signed-off-by: Mikael Persson <mikael.persson@intel.com>
    
        commit b5d7218c244d4355592c25b805da76c2185bf8b1
        Author: Robert Lind <robert.lind@intel.com>
        Date:   Fri May 30 12:56:26 2014 +0200
    
            Camera: Fix orientation of MRD7 back camera
    
            Changes orientation of MRD7 back camera from 90 to 270 degrees.
    
            Issue: GMIN-1225
            Change-Id: I80de3460a68b9f7d1e66787e6d7d941ff2050b48
            Signed-off-by: Robert Lind <robert.lind@intel.com>
    
        commit 5ff8144b55c40be24f0fb4e283f344af290acc09
        Author: Andy Ross <andrew.j.ross@intel.com>
        Date:   Thu May 22 14:30:53 2014 -0700
    
            Add permissions (system:camera 660) for camera devices
    
            Change-Id: I37efe3ee4c9cd3a6d7bf14768aab5f81fd1a09aa
            Issue: GMIN-984
            Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- groups/camera/isp/BoardConfig.mk
+++ groups/camera/isp/BoardConfig.mk
@@ -1,23 +1,17 @@
-INTEL_CAMERA := true
-INTEL_TEST_CAMERA := false
-
-# Use Intel camera extras (HDR, face detection, panorama, etc.) by default
-USE_INTEL_CAMERA_EXTRAS := true
-
-# Camera
-# Set USE_CAMERA_STUB to 'true' for Fake Camera builds,
-# 'false' for libcamera builds to use Camera Imaging(CI) supported by intel.
+# Set USE_CAMERA_STUB to 'true' for Fake Camera builds (emulator).
 USE_CAMERA_STUB := false
 
-# select libcamera2 as the camera HAL
+# Select libcamera2 as the camera HAL
 USE_CAMERA_HAL2 := true
 
-# disable the new V3 HAL by default so it can be added to the tree without conflicts
-# it will be enabled in selected platforms
+# The new camera V3 HAL is work in progress. Do not enable yet.
 USE_CAMERA_HAL_3 := false
 
-# Set USE_VIDEO_EFFECT to 'false' to unsupport live face effect. And Set OMX Component Input Buffer Count to 2.
-USE_VIDEO_EFFECT := true
+# Intel Test Camera depends on Intel camera extension, so we have to set to false for now.
+INTEL_TEST_CAMERA := false
+
+# Intel camera extras (HDR, face detection, panorama, red-eye detection etc.).
+USE_INTEL_CAMERA_EXTRAS := true
 
 # Do not use shared object of ia_face by default
 USE_SHARED_IA_FACE := false
@@ -25,5 +19,18 @@ USE_SHARED_IA_FACE := false
 # Use panorama v1.0 by default
 IA_PANORAMA_VERSION := 1.0
 
+# Set CSS API version. Should be detected in run-time instead, based on HW capability.
 USE_CSS_2_1 := true
+
+# Enable IPP optimized JPEG (backend to libjpeg).
 USE_INTEL_IPP := true
+
+# Disable JPEG HW encoder. Should be detected in run-time instead, based on HW capability.
+USE_INTEL_JPEG := false
+
+# Use Intel meta buffer type to share buffer with video encoder. 
+USE_INTEL_METABUFFER := false
+
+# FIXME: remove below flag when usage has been removed.
+# FIXME2: note that the ufo mixin also sets this, arbitrate...
+BOARD_GRAPHIC_IS_GEN := true
--- groups/camera/isp/product.mk
+++ groups/camera/isp/product.mk
@@ -1,34 +1,35 @@
-#
-# Camera HAL (and dependencies)
-#
+ifeq ($(TARGET_BOARD_PLATFORM),)
+    $(error Please define TARGET_BOARD_PLATFORM in product-level Makefile)
+endif
 
-# libmfldadvci
+# Hald blacklist config
+# REMOVE THIS WHEN hald IS REMOVED
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/../atomisp.conf:system/etc/modprobe.d/atomisp.conf
+
+# Camera HAL (libcamera2)
+PRODUCT_PACKAGES += camera.$(TARGET_BOARD_PLATFORM)
+
+# Kernel module initialization helper script
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/init.atomisp.sh:system/etc/init.atomisp.sh
+
+# 3A/AIQ
 PRODUCT_PACKAGES += \
     libmfldadvci \
     dummy.cpf \
     CGamma_DIS5MP.bin \
     noise.fpn \
-    Preview_UserParameter_imx135.prm \
-    Primary_UserParameter_imx135.prm \
-    Video_UserParameter_imx135.prm \
     YGamma_DIS5MP.bin \
     Mor_8MP_8BQ.txt
 
-ifeq ($(TARGET_BOARD_PLATFORM),)
-    $(error Please define TARGET_BOARD_PLATFORM in product-level Makefile)
-endif
-
-# libcamera
-PRODUCT_PACKAGES += \
-    camera.$(TARGET_BOARD_PLATFORM)
-
 # ISP Firmware
 PRODUCT_PACKAGES += \
-    isp_acc_chromaproc_css21_2400b0.bin \
-    isp_acc_deghosting_v2_css21_2400b0.bin \
-    isp_acc_lumaproc_css21_2400b0.bin \
-    isp_acc_multires_v2_css21_2400b0.bin \
-    isp_acc_warping_v2_css21_2400b0.bin \
+    hdr_v2_fw_css21_2400b0 \
     shisp_2400b0_v21.bin \
 
+# libmix
+PRODUCT_PACKAGES += libmix_videovpp
 
+# Only include libmix_videovpp test program in eng builds.
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_PACKAGES += csc_vpp
+endif
