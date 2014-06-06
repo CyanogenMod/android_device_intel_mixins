cfdc6914d5b32276dc949c475b861c1349d590df
Scott D Phillips <scott.d.phillips@intel.com>
2014-06-06 18:08:32 +0000

    REVERTME: Sleep before starting supplicant to mitigate SystemServer race
    Tracked-by: GMIN-1369
    Change-Id: Id4f8c93991f59d3e9258d26fcc1d8e9dd1d307a6
    Signed-off-by: Scott D Phillips <scott.d.phillips@intel.com>
    Reviewed-on: https://android.intel.com/204424

--- groups/wlan/rtl8723bs/product.mk
+++ groups/wlan/rtl8723bs/product.mk
@@ -1,6 +1,7 @@
 # Uses same supplicant config as the bcm43241
 PRODUCT_COPY_FILES += \
     device/intel/common/wlan/wpa_supplicant-common.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/intel/common/wlan/slow_start_supplicant.sh:system/bin/slow_start_supplicant.sh \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
