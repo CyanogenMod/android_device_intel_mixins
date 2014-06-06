07870bdb3ce78031ad81031c5cb93ffe2e481cdf
Scott D Phillips <scott.d.phillips@intel.com>
2014-06-06 17:23:11 +0000

    Configuration changes to enable wifi direct and wifi display
    Change-Id: I1c1dce8fbee64bbfc5d6ccb72c94df6bb0a1f1c8
    Signed-off-by: Scott D Phillips <scott.d.phillips@intel.com>
    Reviewed-on: https://android.intel.com/204418
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Gumbel, Matthew K <matthew.k.gumbel@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- groups/wlan/rtl8723bs/BoardConfig.mk
+++ groups/wlan/rtl8723bs/BoardConfig.mk
@@ -3,3 +3,4 @@ WPA_SUPPLICANT_VERSION := VER_2_1_DEVEL
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_intc
 
+DEVICE_PACKAGE_OVERLAYS += device/intel/common/wlan/overlay-wifi-display
--- groups/wlan/rtl8723bs/product.mk
+++ groups/wlan/rtl8723bs/product.mk
@@ -1,7 +1,8 @@
 # Uses same supplicant config as the bcm43241
 PRODUCT_COPY_FILES += \
     device/intel/common/wlan/wpa_supplicant-common.conf:system/etc/wifi/wpa_supplicant.conf \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
 PRODUCT_PACKAGES += wpa_supplicant
 
