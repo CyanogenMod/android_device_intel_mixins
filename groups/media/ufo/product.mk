6589dc538c14be8b721b030a6eb61dc091203435
Robert Beatty <robert.m.beatty@intel.com>
2014-06-07 15:47:29 -0700

    Audio: Remove Intel-Accelerated AMR codecs
    Removing all Intel AMR-WB/AMR-NB codecs. Encoders have noise.
    Decoders have timing playback issues for certain file types.
    
    Issue: GMIN-1272
    Issue: GMIN-1005
    Change-Id: Ia1c86f476dc75e9fc0140d73b1e01bef13bd3197
    Depends-Change-Id: I66fc02ae2b38a7d76f4011965682e22dbb36b25b
    Signed-off-by: Robert Beatty <robert.m.beatty@intel.com>
    Reviewed-on: https://android.intel.com/204579
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- groups/media/ufo/product.mk
+++ groups/media/ufo/product.mk
@@ -17,11 +17,8 @@ PRODUCT_PACKAGES += \
 # Build OMX wrapper codecs
 PRODUCT_PACKAGES += \
     libmdp_omx_core \
-    libmc_amrcommon \
     libstagefright_soft_mp3dec_mdp \
     libstagefright_soft_aacdec_mdp \
     libstagefright_soft_aacenc_mdp \
-    libstagefright_soft_vorbisdec_mdp \
-    libstagefright_soft_amrdec_mdp \
-    libstagefright_soft_amrenc_mdp
+    libstagefright_soft_vorbisdec_mdp
 
