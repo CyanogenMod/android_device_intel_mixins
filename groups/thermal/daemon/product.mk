39adee3c972b203cfef5d39c1d8e0c79d9f42dc0
Radivoje Jovanovic <radivoje.jovanovic@intel.com>
2014-06-11 15:04:33 -0700

    add thermal daemon xml files to PRODUCT_COPY_FILES
    Change-Id: I2070ebacf5c11eea1c7c2e854c8019248175ca79
    Signed-off-by: Radivoje Jovanovic <radivoje.jovanovic@intel.com>
    Reviewed-on: https://android.intel.com/205950
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Leung, Daniel <daniel.leung@intel.com>

--- groups/thermal/daemon/product.mk
+++ groups/thermal/daemon/product.mk
@@ -1,3 +1,6 @@
 # Thermal daemon
 PRODUCT_PACKAGES += thermal-daemon
 
+PRODUCT_COPY_FILES += \
+    external/thermal_daemon/data/thermal-conf.xml:system/etc/thermal-daemon/thermal-conf.xml \
+    external/thermal_daemon/data/thermal-cpu-cdev-order.xml:system/etc/thermal-daemon/thermal-cpu-cdev-order.xml
