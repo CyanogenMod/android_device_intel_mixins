43a279fa5f77a87b40738e8163bc630c6ddf628f
Matt Gumbel <matthew.k.gumbel@intel.com>
2014-06-13 17:13:06 -0700

    graphics: add software option
    Let android believe it runs in the emulator without support for GPU
    acceleration.
    
    Change-Id: Ic595393c658384f411622464a79228bf64b400e5
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>
    Reviewed-on: https://android.intel.com/219621
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Tested-by: Boie, Andrew P <andrew.p.boie@intel.com>

---
+++ groups/graphics/software/BoardConfig.mk
@@ -0,0 +1,14 @@
+BOARD_KERNEL_CMDLINE += vga=current nomodeset
+USE_OPENGL_RENDERER := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+USE_INTEL_UFO_DRIVER := false
+INTEL_VA := true
+BOARD_GRAPHIC_IS_GEN := true
+
+# System's VSYNC phase offsets in nanoseconds
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+
+# Allow HWC to perform a final CSC on virtual displays
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+
