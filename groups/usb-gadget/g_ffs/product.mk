11789bf5b03baf4cf864e31ec4cac33b06e22982
Constantin Musca <constantin.musca@intel.com>
2014-06-20 12:30:16 +0300

    usb-gadget: enable adb over USB using g_ffs kernel driver
    Author: Matt Gumbel <matthew.k.gumbel@intel.com>
    Author: Jianxun Zhang <jianxun.zhang@intel.com>
    Depends-Change-Id: I532966aa3a1d2586554ea10c8d7ad1cb8d82f0a5
    Change-Id: I99cd0db6c07dec4d633a5705cb308635b0de1bca
    Signed-off-by: Constantin Musca <constantin.musca@intel.com>
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>

--- groups/usb-gadget/g_ffs/product.mk
+++ groups/usb-gadget/g_ffs/product.mk
@@ -1,6 +1,9 @@
-# hard code Intel VID and a fake PID here before
-# we have a nicer solution to organize IDs. These
-# parameters are for g_ffs driver built in kernel.
+# hard code Intel VID and PID here. These parameters are for g_ffs driver
+# built in kernel.
+#
+# <arjan> we have an official assigned intel ID
+# <arjan> 0x0a21
+
 BOARD_KERNEL_CMDLINE += g_ffs.idVendor=0x8087 \
-		g_ffs.idProduct=0x1234 \
+		g_ffs.idProduct=0x0a21 \
 
