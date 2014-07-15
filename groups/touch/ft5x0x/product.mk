40ec3ef11b14076c070359e0171ad919abe3284f
Brian Wood <brian.j.wood@intel.com>
2014-07-15 14:50:19 -0700

    Add touchscreen mixin components for ECS touchscreen
    This adds in the touchscreen mixin components for the
    ECS "ft5x0x" touchscreen.
    
    Change-Id: Ica1ab3b47566cd6557dcb9b505d5066840fe0b16
    Signed-off-by: Brian Wood <brian.j.wood@intel.com>

---
+++ groups/touch/ft5x0x/product.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+        device/intel/common/touch/ft5x0x.idc:system/usr/idc/ft5x0x.idc \
+        frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
