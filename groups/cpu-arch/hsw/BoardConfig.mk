0fcdb8dbe469323f511dcaabe110924c37c87eea
Aaron Gascoigne <aaron.gascoigne@intel.com>
2014-07-18 12:33:14 -0700

    Cpu-arch: Enable SMP on all platforms
    Change-Id: Iaca33bd2909a313359a104f18d8dfd6e8b657e7b
    Signed-off-by: Aaron Gascoigne <aaron.gascoigne@intel.com>
    Reviewed-on: https://android.intel.com/220011
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Tested-by: Boie, Andrew P <andrew.p.boie@intel.com>

--- groups/cpu-arch/hsw/BoardConfig.mk
+++ groups/cpu-arch/hsw/BoardConfig.mk
@@ -1,5 +1,6 @@
 TARGET_CPU_ABI := x86
 TARGET_ARCH_VARIANT := haswell
-
 TARGET_ARCH := x86
+TARGET_CPU_SMP := true
+
 
--- groups/cpu-arch/ivb/BoardConfig.mk
+++ groups/cpu-arch/ivb/BoardConfig.mk
@@ -1,5 +1,5 @@
 TARGET_CPU_ABI := x86
 TARGET_ARCH_VARIANT := ivybridge
-
 TARGET_ARCH := x86
+TARGET_CPU_SMP := true
 
