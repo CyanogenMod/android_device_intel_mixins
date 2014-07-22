3aef9a04e102d10ed7712727867ba15912d5468c
Alexandru Timohi <alexandru.timohi@intel.com>
2014-07-22 14:12:28 +0300

    gps: bcm4752: implement RfType and FrqPlan properties
    Implement the ro.spid.gps.RfType and ro.spid.gps.FrqPlan
    overriding values from gps.xml for scalability reasons.
    
    This change applies to BCM SW rls 205486 onwards.
    
    Enable TCXO clk calibration from MOFD PR1.
    
    Depends-Change-Id: If9dc631cb3750a1e7c41970a9e51f1a4baa5f081
    Change-Id: I69e41e984d73885e262199ae28938bfc4a58fdcc
    Signed-off-by: Alexandru Timohi <alexandru.timohi@intel.com>
    Reviewed-on: https://android.intel.com/220696

--- groups/gps/bcm4752/product.mk
+++ groups/gps/bcm4752/product.mk
@@ -6,6 +6,10 @@ PRODUCT_PACKAGES += \
     gpsd \
     gpscerd \
 
+PRODUCT_DEFAULT_PROOPERTY_OVERRIDES += \
+        ro.spid.gps.FrqPlan=FRQ_PLAN_26MHZ_2PPM_26MHZ_100PPB \
+        ro.spid.gps.RfType=GL_RF_47521_BRCM
+
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 
