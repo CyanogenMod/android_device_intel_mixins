1fe5ba1c2e8eb1e9af89daddf6015b1184979043
Jianxun Zhang <jianxun.zhang@intel.com>
2014-08-13 14:54:44 -0700

    update USB PID to 09EF
    This ID is required for OS provisioning mode.
    
    Depends-Change-Id: I73dd7cb4b2302ae03c903496491cd7025cc6ff29
    Change-Id: I018349be0189cef9a029510ef8890c27a02fb499
    Signed-off-by: Jianxun Zhang <jianxun.zhang@intel.com>
    Reviewed-on: https://android.intel.com/228894
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Tested-by: Boie, Andrew P <andrew.p.boie@intel.com>

--- groups/usb-gadget/g_ffs/product.mk
+++ groups/usb-gadget/g_ffs/product.mk
@@ -1,9 +1,7 @@
 # hard code Intel VID and PID here. These parameters are for g_ffs driver
 # built in kernel.
 #
-# <arjan> we have an official assigned intel ID
-# <arjan> 0x0a21
 
 BOARD_KERNEL_CMDLINE += g_ffs.idVendor=0x8087 \
-		g_ffs.idProduct=0x0a21 \
+		g_ffs.idProduct=0x09ef \
 
