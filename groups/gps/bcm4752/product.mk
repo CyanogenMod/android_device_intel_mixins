5678cad6f589c019c63a4688d425bcf2d823aa0d
Alexandru Timohi <alexandru.timohi@intel.com>
2014-08-14 09:02:26 +0300

    baytrail: gps: Create RfType and Frqplan parameters
    RfType and Frqplan will become paramaters for mixins
    
    Change-Id: Ia52b49972802e5c42f17ee4a7cb2c14404cdf6f5
    Signed-off-by: Alexandru Timohi <alexandru.timohi@intel.com>
    Reviewed-on: https://android.intel.com/227854
    Reviewed-by: Dolca, Robert <robert.dolca@intel.com>
    Tested-by: Dolca, Robert <robert.dolca@intel.com>

--- groups/gps/bcm4752/product.mk
+++ groups/gps/bcm4752/product.mk
@@ -6,9 +6,9 @@ PRODUCT_PACKAGES += \
     gpsd \
     gpscerd \
 
-PRODUCT_DEFAULT_PROOPERTY_OVERRIDES += \
-        ro.spid.gps.FrqPlan=FRQ_PLAN_26MHZ_2PPM_26MHZ_100PPB \
-        ro.spid.gps.RfType=GL_RF_47521_BRCM
+PRODUCT_PROOPERTY_OVERRIDES += \
+        ro.spid.gps.FrqPlan=$[FrqPlan]\
+        ro.spid.gps.RfType=$[RfType]
 
 PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
