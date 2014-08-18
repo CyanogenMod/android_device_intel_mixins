b90c82f30afd664459b797ef3d8b81f96383acbb
Mattias Pettersson <mattias.pettersson@intel.com>
2014-08-18 18:43:13 +0200

    sepolicy: add .te files
    Include all new sepolicy .te files.
    
    Change-Id: Ic3b3f9546eba6a0f800fc6215cd5255a6edfca96
    Depends-Change-Id: I5e5927a1ae47196aa6d95c14cfdd3f23da289aa3
    Signed-off-by: Mattias Pettersson <mattias.pettersson@intel.com>
    Reviewed-on: https://android.intel.com:443/229488

--- groups/sepolicy/intel/BoardConfig.mk
+++ groups/sepolicy/intel/BoardConfig.mk
@@ -1,11 +1,25 @@
 BOARD_SEPOLICY_DIRS := device/intel/common/sepolicy
+BOARD_SEPOLICY_REPLACE := \
+    domain.te \
+    init.te \
+    kernel.te
 BOARD_SEPOLICY_UNION := \
     genfs_contexts \
     file_contexts \
     file.te \
+    bluetooth.te \
     device.te \
+    dhcp.te \
+    gpsd.te \
+    init_shell.te \
+    keystore.te \
+    mediaserver.te \
     netd.te \
     pstore-clean.te \
-    userfastboot.te \
-    vold.te
-
+    sdcardd.te \
+    surfaceflinger.te \
+    system_server.te \
+    userfastboot.te  \
+    vold.te \
+    wpa.te \
+    setup_fs.te
