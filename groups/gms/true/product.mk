f5c3b33c377576148dddb6d054bc1ac6a572f936
Piotr Tomaszewski <piotr.tomaszewski@intel.com>
2014-08-19 13:11:57 +0200

    Configure clientidbase
    Set ro.com.google.clientidbase to android-intel as
    required by the GMS integration. It is used by the
    GMS applications to generate the actual client IDs
    for different GMS applications.
    
    Change-Id: Ic8240e81af21483fda66191f06c66ce7c406c7ec
    Reviewed-on: https://android.intel.com/229837
    Reviewed-by: Tomaszewski, Piotr <piotr.tomaszewski@intel.com>
    Reviewed-by: Zhang, Guobin <guobin.zhang@intel.com>
    Tested-by: Tomaszewski, Piotr <piotr.tomaszewski@intel.com>
    Reviewed-by: Fagerstedt, Axel <axel.fagerstedt@intel.com>

--- groups/gms/true/product.mk
+++ groups/gms/true/product.mk
@@ -1,2 +1,5 @@
 $(call inherit-product-if-exists, vendor/google/gms/products/gms.mk)
 $(call inherit-product-if-exists, vendor/google/gms/products/gms_optional.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+ ro.com.google.clientidbase=android-intel
