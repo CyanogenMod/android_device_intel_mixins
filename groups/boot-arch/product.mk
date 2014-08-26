ebd83eb6b9d76ff0a4c3e79da41e96c98a42e8d2
Vikas Gupta <vikasx1.gupta@intel.com>
2014-08-25 17:00:30 -0700

    Change in variable name to align with AOSP changes.
    Issue: GMINL-717
    Change-Id: Id724cefac1c5052f5b8e735c20b44c636482f15f
    Signed-off-by: Vikas Gupta <vikasx1.gupta@intel.com>
    Reviewed-on: https://android.intel.com/233819
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Tested-by: Boie, Andrew P <andrew.p.boie@intel.com>

--- groups/boot-arch/product.mk
+++ groups/boot-arch/product.mk
@@ -1,6 +1,6 @@
 $(call inherit-product,build/target/product/verity.mk)
 
-PRODUCT_VERITY_PARTITION := /dev/block/by-name/android_system
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/by-name/android_system
 
 PRODUCT_PACKAGES += \
 	setup_fs \
