e6a19960913914c7dae91d10e8d39068a3c36034
Eduard Lataretu <eduard.lataretu@intel.com>
2014-10-02 12:24:24 +0300

    Refactor rfkill service location
    This patch moves the rfkill service creation and initialization
    from the bluetooth to its own mixin group in order to have a
    scalable solution for multiple components.
    
    Change-Id: Ifb810acc9638232f06c6a398174610d8ec0ae220
    Depends-Change-Id: Ie02cb03ea1e047b41ebb13f772548d75ad6d629d
    Depends-Change-Id: Ib08d6a44291f7c2adfdaccda3b65aa614f69e3bb
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-1651
    Signed-off-by: Eduard Lataretu <eduard.lataretu@intel.com>
    Reviewed-on: https://android.intel.com/283058
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>
    Reviewed-by: Chiras, Robert <robert.chiras@intel.com>
    Reviewed-by: Deprez, Olivier <olivier.deprez@intel.com>

--- groups/bluetooth/rtl8723bs/product.mk
+++ groups/bluetooth/rtl8723bs/product.mk
@@ -7,7 +7,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
 		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
-PRODUCT_COPY_FILES += device/intel/common/bluetooth/rfkill-init/init.rfkill.sh:system/etc/init.rfkill.sh
-
 ADDITIONAL_BUILD_PROPERTIES += bluetooth.hwcfg=stop \
                 bluetooth.rfkill=1
---
+++ groups/rfkill/true/product.mk
@@ -0,0 +1,1 @@
+PRODUCT_COPY_FILES += device/intel/common/rfkill/rfkill-init.sh:system/etc/rfkill-init.sh
