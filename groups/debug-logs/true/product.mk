c264e8a4d5227730d4634a95b32a83b4cad321f8
Jeremy Rocher <jeremy.rocher@intel.com>
2014-10-16 18:53:38 +0200

    debug-logs: add logcatext dependency
    logcatext is used as ap_logfs service.
    
    Change-Id: I62c816ef4cbaff08ed010f52641e6e56c735b6a0
    Tracked-On: https://jira01.devtools.intel.com/browse/AREQ-3238
    Signed-off-by: Jeremy Rocher <jeremy.rocher@intel.com>
    Reviewed-on: https://android.intel.com/289417
    Reviewed-by: Kumar-mayernik, Nisha <nisha.kumar-mayernik@intel.com>
    Tested-by: Kumar-mayernik, Nisha <nisha.kumar-mayernik@intel.com>

--- groups/debug-logs/true/product.mk
+++ groups/debug-logs/true/product.mk
@@ -1,4 +1,5 @@
 # Enable logs on file system for eng and userdebug builds
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_COPY_FILES += device/intel/common/debug/init.logs.rc:root/init.logs.rc
+PRODUCT_PACKAGES += logcatext
 endif
