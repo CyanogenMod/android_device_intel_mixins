44faee181de84000faa50ca992c47ec4caca0cc8
Kenny (Wuchun) Gong <wuchun.gong@intel.com>
2014-10-27 19:18:22 +0800

    Mixin update for GMS integration
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-3113
    
    Change-Id: Ic7aae7723058c81b737fab8596898197ad0851b7
    Signed-off-by: Kenny (Wuchun) Gong <wuchun.gong@intel.com>
    Reviewed-on: https://android.intel.com/293375
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Tomaszewski, Piotr <piotr.tomaszewski@intel.com>
    Reviewed-by: Fagerstedt, Axel <axel.fagerstedt@intel.com>
    Tested-by: Fagerstedt, Axel <axel.fagerstedt@intel.com>

--- groups/gms/true/BoardConfig.mk
+++
@@ -1,3 +0,0 @@
-DEVICE_PACKAGE_OVERLAYS += device/intel/common/gms/overlay
-FLAG_GMS_AVAILABLE := yes
-
--- groups/gms/true/product.mk
+++ groups/gms/true/product.mk
@@ -1,6 +1,4 @@
-PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
-
-$(call inherit-product-if-exists, vendor/google/gms/products/gms_gpe.mk)
+$(call inherit-product-if-exists, vendor/google/gms/products/gms_full.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
  ro.com.google.clientidbase=android-intel
