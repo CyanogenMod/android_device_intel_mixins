a50c8ced5f7376531195b14ceacdf08656955646
Kenny (Wuchun) Gong <wuchun.gong@intel.com>
2014-10-29 05:46:14 +0800

    Mixin update for GMS 5.0 integration
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-3261
    
    Change-Id: I84f74f2d6569e93b97061d5b69c1f4860b3367f5
    Signed-off-by: Kenny (Wuchun) Gong <wuchun.gong@intel.com>
    Reviewed-on: https://android.intel.com/294327
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Tomaszewski, Piotr <piotr.tomaszewski@intel.com>
    Reviewed-by: Fagerstedt, Axel <axel.fagerstedt@intel.com>
    Tested-by: Fagerstedt, Axel <axel.fagerstedt@intel.com>

--- groups/gms/true/product.mk
+++ groups/gms/true/product.mk
@@ -1,4 +1,4 @@
-$(call inherit-product-if-exists, vendor/google/gms/products/gms_full.mk)
+$(call inherit-product-if-exists, vendor/google/gms/products/gms_gpe.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
  ro.com.google.clientidbase=android-intel
