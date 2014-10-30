0d306290ef8eba72def9407c7e740bd5017ba309
Claude Brouat <claude.brouat@intel.com>
2014-10-30 18:57:26 +0100

    Simplify kernel mixin options
    Add parameters to kernel mixin to simplify mixin
    code and limit duplications.
    
    Change-Id: Id74dce221fbf724256040f43b651b26761b307fe
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-3388
    Signed-off-by: Claude Brouat <claude.brouat@intel.com>
    Reviewed-on: https://android.intel.com:443/295062

--- groups/kernel/BoardConfig.mk
+++ groups/kernel/BoardConfig.mk
@@ -1,7 +1,7 @@
 # Specify location of board-specific kernel headers
-TARGET_BOARD_KERNEL_HEADERS := device/intel/gmin-kernel/kernel-headers
+TARGET_BOARD_KERNEL_HEADERS := device/intel/$[path]-kernel/kernel-headers
 
-KERNEL_LOGLEVEL ?= 5
+KERNEL_LOGLEVEL ?= $[loglevel]
 
 BOARD_KERNEL_CMDLINE += \
         loglevel=$(KERNEL_LOGLEVEL) \
