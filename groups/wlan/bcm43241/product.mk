fc08766dbb4fef7480dc9a261d86cb7751e77e9e
Jean Trivelly <jean.trivelly@intel.com>
2014-11-05 15:33:18 +0100

    WiFi: Enable P2P on bcm43241.
    To enable P2P, it is required to specify p2p_supplicant service
    and to add wifi-direct.xml into the target.
    In order to add some wifi parameters required for intel platform,
    it is required to use overlay mechanism provided by supplicant.
    
    Change-Id: I1bf615b4c4cbee54fae27462400d95961be262c4
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-3653
    Signed-off-by: Jean Trivelly <jean.trivelly@intel.com>
    Reviewed-on: https://android.intel.com:443/300277

--- groups/wlan/bcm43241/product.mk
+++ groups/wlan/bcm43241/product.mk
@@ -1,6 +1,8 @@
 PRODUCT_COPY_FILES += \
     device/intel/common/wlan/wpa_supplicant-common.conf:system/etc/wifi/wpa_supplicant.conf \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+    device/intel/common/wlan/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
 PRODUCT_PACKAGES += wpa_supplicant \
     brcmfmac43241b4-sdio.bin \
