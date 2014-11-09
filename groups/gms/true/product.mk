889db61c8fca2ba46dac596715acb642343125ff
Axel Fagerstedt <axel.fagerstedt@intel.com>
2014-11-09 20:45:38 +0100

    Add homescreen customization activity
    Change-Id: I1172f0f9ab93c2026700a2525f0f52cb2b91a279
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-3523
    Reviewed-on: https://android.intel.com/298849
    Reviewed-by: Fagerstedt, Axel <axel.fagerstedt@intel.com>
    Tested-by: Fagerstedt, Axel <axel.fagerstedt@intel.com>

--- groups/gms/true/product.mk
+++ groups/gms/true/product.mk
@@ -1,4 +1,6 @@
 $(call inherit-product-if-exists, vendor/google/gms/products/gms_gpe.mk)
 
+PRODUCT_PACKAGES += DefaultLayout
+
 PRODUCT_PROPERTY_OVERRIDES += \
  ro.com.google.clientidbase=android-intel
