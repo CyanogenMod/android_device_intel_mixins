6388245909941610a43ff1e918c39b76961997cf
Jonathan DE CESCO <jonathan.de.cesco@intel.com>
2014-11-18 13:51:28 +0100

    Wlan: bcm43241 modify init/conf files to enable Portable HotSpot
    Enable Portable HotSpot feature by adding hostapd binary
    and conf file to the build.
    Also define station/p2p and hotspot firmware paths
    
    Change-Id: I114ba07ec0e11489322eeb84d1b2fd068a71364d
    Signed-off-by: Jonathan DE CESCO <jonathan.de.cesco@intel.com>
    Reviewed-on: https://android.intel.com:443/302173

--- groups/wlan/bcm43241/BoardConfig.mk
+++ groups/wlan/bcm43241/BoardConfig.mk
@@ -1,7 +1,12 @@
 ADDITIONAL_DEFAULT_PROPERTIES += wifi.interface=wlan0
 # FIXME: use VER_0_8_X so lib_driver_cmd_bcmdhd can be built
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_PRIVATE_LIB      := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER           := NL80211
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WLAN_DEVICE := bcmdhd
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/brcm/brcmfmac43241b4-sdio.bin"
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/brcm/brcmfmac43241b4-sdio_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/vendor/firmware/brcm/brcmfmac43241b4-sdio.bin"
--- groups/wlan/bcm43241/product.mk
+++ groups/wlan/bcm43241/product.mk
@@ -1,10 +1,13 @@
 PRODUCT_COPY_FILES += \
     device/intel/common/wlan/wpa_supplicant-common.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/intel/common/wlan/hostapd.conf:system/etc/wifi/hostapd.conf \
     device/intel/common/wlan/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
 PRODUCT_PACKAGES += wpa_supplicant \
+    hostapd \
     brcmfmac43241b4-sdio.bin \
+    brcmfmac43241b4-sdio_apsta.bin \
     brcmfmac43241b4-sdio.txt \
     brcmfmac43241b4-oob-sdio.txt
