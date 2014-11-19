5a747778358150e2d1ef5db2d2435fbe374e66ad
Robert Dolca <robert.dolca@intel.com>
2014-11-19 18:23:21 +0200

    bluetooth: USB dongle support added
    Change-Id: I08d4da0ec6aff2130fac1d1698b092e4ab96102a
    Signed-off-by: Robert Dolca <robert.dolca@intel.com>
    Reviewed-on: https://android.intel.com/303078
    Tested-by: cactus <cactus@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Dobre, Antonel G <antonel.g.dobre@intel.com>
    Reviewed-by: Pircalabu, Petre Ovidiu <petre.ovidiu.pircalabu@intel.com>
    Tested-by: Pircalabu, Petre Ovidiu <petre.ovidiu.pircalabu@intel.com>

---
+++ groups/bluetooth/btusb/product.mk
@@ -0,0 +1,8 @@
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
+ADDITIONAL_BUILD_PROPERTIES += bluetooth.hwcfg=stop \
+                bluetooth.rfkill=1
