942e8f991969fdf8f1c7df893ad52407c97f100e
Andrew Boie <andrew.p.boie@intel.com>
2014-11-20 10:12:51 -0800

    boot-arch/efi: fix build breakage if capsule not present
    Change-Id: I63ecd5f4daa970e187955bc8d82e88af19a4b5e9
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/303579

--- groups/boot-arch/efi/product.mk
+++ groups/boot-arch/efi/product.mk
@@ -20,10 +20,6 @@ BOARD_SFU_UPDATE := hardware/intel/efi_capsules/$(BIOS_VARIANT)/$(TARGET_PRODUCT
 EFI_IFWI_BIN := hardware/intel/efi_capsules/$(BIOS_VARIANT)/$(TARGET_PRODUCT)_ifwi.bin
 EFI_EMMC_BIN := hardware/intel/efi_capsules/$(BIOS_VARIANT)/$(TARGET_PRODUCT)_emmc.bin
 
-ifneq ($(BOARD_SFU_UPDATE),)
-PRODUCT_COPY_FILES += $(BOARD_SFU_UPDATE):system/etc/firmware/BIOSUPDATE.fv
-endif
-
 ifneq ($(CALLED_FROM_SETUP),true)
 ifeq ($(wildcard $(BOARD_SFU_UPDATE)),)
 $(warning $(BOARD_SFU_UPDATE) not found, OTA updates will not provide a firmware capsule)
@@ -37,5 +33,9 @@ ifeq ($(wildcard $(EFI_IFWI_BIN)),)
 $(warning $(EFI_IFWI_BIN) not found, IFWI binary will not be provided in out/dist/)
 EFI_IFWI_BIN :=
 endif
+endif
+
+ifneq ($(BOARD_SFU_UPDATE),)
+PRODUCT_COPY_FILES += $(BOARD_SFU_UPDATE):system/etc/firmware/BIOSUPDATE.fv
 endif
 
