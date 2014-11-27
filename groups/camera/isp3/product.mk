a6334fe2b9934e66a493c79c1b3b300eba1ddaec
Mikael Persson <mikael.persson@intel.com>
2014-11-27 14:11:14 +0100

    camera cleanups
    All device/board specific files are now handled from
    device-specific configuration files instead.
    
    Change-Id: I15eafeb056520cfc080380ca1ab139e5cf860f7e
    Signed-off-by: Mikael Persson <mikael.persson@intel.com>

--- groups/camera/isp3/product.mk
+++ groups/camera/isp3/product.mk
@@ -2,25 +2,11 @@ ifeq ($(TARGET_BOARD_PLATFORM),)
     $(error Please define TARGET_BOARD_PLATFORM in product-level Makefile)
 endif
 
-# Android framework boilerplate.
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-
-# HAL config file
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/camera3_profiles.xml:system/etc/camera3_profiles.xml
-
-# Camera HAL (camera3hal)
+# Camera HAL (camera3hal).
 PRODUCT_PACKAGES += camera.$(TARGET_BOARD_PLATFORM)
 
-# Kernel module initialization helper script
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/init.atomisp.sh:system/etc/init.atomisp.sh
-
-# 3A/AIQ
+# 3A/AIQ and camera tuning files.
 PRODUCT_PACKAGES += \
     libmfldadvci \
     dummy.aiqb \
 
-# ISP Firmware
-PRODUCT_PACKAGES += \
-    shisp_2400b0_v21.bin \
