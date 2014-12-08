d9687cdca09a6f108f4c9193e9533de25091cd95
Traian Schiau <traianx.schiau@intel.com>
2014-12-08 15:35:20 +0100

    mixins: add crashlogd policy to gmin
    Change-Id: I17363a46ebcdd1b9cf3ca03e05028d10bbb30a90
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-3394
    Signed-off-by: Traian Schiau <traianx.schiau@intel.com>
    Signed-off-by: Nicolae Natea <nicolaex.natea@intel.com>
    Reviewed-on: https://android.intel.com:443/309272

--- groups/debug-crashlogd/true/BoardConfig.mk
+++ groups/debug-crashlogd/true/BoardConfig.mk
@@ -1,5 +1,12 @@
 # Enable crashlogd for eng and userdebug builds
 ifneq ($(TARGET_BUILD_VARIANT),user)
+
+BOARD_SEPOLICY_DIRS += device/intel/common/sepolicy/crashlogd
+
+BOARD_SEPOLICY_UNION += \
+	crashlogd.te \
+	file_contexts
+
 CRASHLOGD_LOGS_PATH := "/data/logs"
 CRASHLOGD_APLOG := true
 CRASHLOGD_FULL_REPORT := true
