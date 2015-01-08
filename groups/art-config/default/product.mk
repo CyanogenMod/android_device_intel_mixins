92dc8a01d7cdea2367073575846ca4e310f18858
Nitheesh K L <nitheesh.k.l@intel.com>
2015-01-08 12:49:58 -0800

    Revert "disable art-extension temporarily for latest MR1 rebase"
    This reverts commit b9b28cb6527c9a66aad6bcacc4b671dba5a6bea4.
    
    Reverting this commit to re-enable art-extension
    
    Change-Id: Iaf9535d02335d9ec93ff73f52ff9068e15c3df4f

--- groups/art-config/default/product.mk
+++ groups/art-config/default/product.mk
@@ -1,4 +1,4 @@
 # This is needed to enable silver art optimizer.
 # This will build the plugins/libart-extension.so library,  which is dynamically loaded by
 # AOSP and contains Intel optimizations to the compiler.
-#PRODUCT_PACKAGES += libart-extension
+PRODUCT_PACKAGES += libart-extension
