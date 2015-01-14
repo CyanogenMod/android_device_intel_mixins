33ba3bd0a81a22185d6f6bab5a4fe75f03ed1430
Jean Trivelly <jean.trivelly@intel.com>
2015-01-14 17:42:14 +0100

    Add tool to restore WiFi NVM from GPP/GPT partition to /config/wlan
    At each boot, restore WiFi NVM from GPP/GPT partition to /config/wlan.
    If GPP/GPT partition is empty, copy the default NVM, generate a mac addr
    and populate the GPP/GPT with it.
    
    Change-Id: Ia829c09d0b7604a4b85b2c3a7defd74d6f732b2d
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-16941
    Signed-off-by: Jean Trivelly <jean.trivelly@intel.com>

--- groups/wlan/iwlwifi/product.mk
+++ groups/wlan/iwlwifi/product.mk
@@ -14,5 +14,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
         device/intel/common/wlan/iwlwifi/load_iwlwifi.sh:system/bin/load_iwlwifi.sh
 
+
 $(call inherit-product-if-exists, vendor/intel/fw/iwl/iwl-fw-{{{chip_id}}}.mk)
 
+# Add Manufacturing tool
+PRODUCT_PACKAGES += \
+    wlan_intel_restore.sh
