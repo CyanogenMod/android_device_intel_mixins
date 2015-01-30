de0a6969239257dae49fe79388857ad770c7da8d
nbertran <nicolas.bertrand@intel.com>
2015-01-30 11:36:44 +0100

    bluetooth: Add debug tools
    Update mixins to include bluetooth tools
    for eng and userdebug variant build.
    Mixins impacted: bcm43241, rtl8723bs
    
    Change-Id: I7ecaf85b8ce9cdc49a1a800629ed14bfa7afc861
    Issue: https://jira01.devtools.intel.com/browse/GMINL-5040
    Signed-off-by: nbertran <nicolas.bertrand@intel.com>
    Reviewed-on: https://android.intel.com/331639
    Reviewed-by: Dalleau, Frederic <frederic.dalleau@intel.com>
    Tested-by: Dalleau, Frederic <frederic.dalleau@intel.com>

--- groups/bluetooth/rtl8723bs/product.mk
+++ groups/bluetooth/rtl8723bs/product.mk
@@ -9,3 +9,11 @@ PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:
 
 ADDITIONAL_BUILD_PROPERTIES += bluetooth.hwcfg=stop \
                 bluetooth.rfkill=1
+
+# Bluetooth eng / userdebug
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += \
+	hciconfig \
+	btmon \
+	hcitool
+endif
