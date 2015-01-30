9396f895053615cedf31cc765ce2490575426025
Axel Fagerstedt <axel.fagerstedt@intel.com>
2015-01-30 13:13:16 +0100

    Add support for CTS automation build
    But this time do it properly.
    
    Change-Id: I36f79499dbaf3afd46092afe40cc44f570b4e2eb
    Reviewed-on: https://android.intel.com:443/325915

--- groups/usb-gadget/g_android/product.mk
+++ groups/usb-gadget/g_android/product.mk
@@ -1,5 +1,11 @@
 # Set default USB interface
+ifeq ($(BUILD_FOR_CTS_AUTOMATION),true)
+# Build for automated CTS
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp,adb
+PRODUCT_COPY_FILES += device/intel/common/usb-gadget/adb_keys:root/adb_keys
+else
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp
+endif
 
 # Enable Secure Debugging
 ifneq ($(TARGET_BUILD_VARIANT),eng)
