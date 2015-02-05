cf81a471e9424372f7605c0365f01ed7fc769afc
Mikael Persson <mikael.persson@intel.com>
2015-02-05 13:57:17 +0100

    camera:ipu2: use version parameter to indicate IPU2 variant.
    When using ipu2 mixin, you now have to state which IPU2 variant
    to use. This is needed to know which ISP FW to use.
    
    Change-Id: I6ff64fc3637bd4134e2e18b9c82ec50b81a0d1cb
    Signed-off-by: Mikael Persson <mikael.persson@intel.com>
    Reviewed-on: https://android.intel.com/329039
    Reviewed-by: Fredriksson, John <john.fredriksson@intel.com>
    Tested-by: Fredriksson, John <john.fredriksson@intel.com>

--- groups/camera/ipu2/product.mk
+++ groups/camera/ipu2/product.mk
@@ -8,7 +8,7 @@ PRODUCT_PACKAGES += camera.$(TARGET_BOARD_PLATFORM)
 # 3A/AIQ and camera tuning files.
 PRODUCT_PACKAGES += \
     libmfldadvci \
-    dummy.aiqb \
+    dummy.aiqb
 
 # Only include test apps in eng builds.
 PRODUCT_PACKAGES_ENG += \
