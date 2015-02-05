ce53ec774f4b85ba5e1bb17abe289da1b65c8521
Benzi Zbit <benzi.zbit@intel.com>
2015-02-05 17:05:05 +0200

    iwlwifi: Add SoftAp FW reload definitions
    fixing SoftAp enablement issue.
    we don't really need this, it's to avoid error when the framework
    will trigger the fwReloadSoftap function, what will lead to an error
    enabling the SoftAp.
    so we set up this for letting the function execute gracefully.
    
    Signed-off-by: Benzi Zbit <benzi.zbit@intel.com>
    Change-Id: I77addd8b0c51dab5373677fecf2a82f99f7056a9
    Reviewed-on: https://android.intel.com/329047
    Reviewed-by: Poulain, Loic <loic.poulain@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>
    Reviewed-by: Dalleau, Frederic <frederic.dalleau@intel.com>

--- groups/wlan/iwlwifi/BoardConfig.mk
+++ groups/wlan/iwlwifi/BoardConfig.mk
@@ -5,3 +5,13 @@ WPA_SUPPLICANT_VERSION := VER_2_1_DEVEL_WCS
 
 # Enabling iwlwifi
 BOARD_USING_INTEL_IWL := true
+
+# SoftAp FW reload definitions.
+# we don't really need this, it's to avoid error when the framework
+# will trigger the fwReloadSoftap function, what will lead to an error
+# enabling the SoftAp.
+# so we set up this for letting the function execute gracefully.
+WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/iwlwifi-softap-dummy.ucode"
+WIFI_DRIVER_FW_PATH_AP  := "/system/vendor/firmware/iwlwifi-softap-dummy.ucode"
+WIFI_DRIVER_FW_PATH_P2P := "/system/vendor/firmware/iwlwifi-softap-dummy.ucode"
+WIFI_DRIVER_FW_PATH_PARAM := "/dev/null"
