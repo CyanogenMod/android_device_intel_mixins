a0e93dcdb4f41eeaa772c0bc44496f6aa9c76e74
Guillaume Lucas <guillaume.lucas@intel.com>
2015-02-19 17:19:25 +0100

    mixins: reworked ims mixins
    Change-Id: I9507d0e0f3126b548e7d16eef11c8379e15e6cb5
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-22765
    Signed-off-by: Guillaume Lucas <guillaume.lucas@intel.com>
    Reviewed-on: https://android.intel.com:443/333971
    Reviewed-on: https://android.intel.com/336407
    Reviewed-by: Svenberg, Stefan <stefan.svenberg@intel.com>
    Tested-by: Svenberg, Stefan <stefan.svenberg@intel.com>

--- groups/ims/bp_centric/BoardConfig.mk
+++ groups/ims/bp_centric/BoardConfig.mk
@@ -1,1 +1,6 @@
-../common/BoardConfig.mk
+# IMS overlay folder
+DEVICE_PACKAGE_OVERLAYS += device/intel/common/ims/overlay
+
+# IMS Properties
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.ims_support=1
--- groups/ims/common/BoardConfig.mk
+++
@@ -1,2 +0,0 @@
-# IMS overlay folder
-DEVICE_PACKAGE_OVERLAYS += device/intel/common/ims/overlay
