ae64d32d1515a5a27f2b787a602a5bcb64747224
Even Xu <even.xu@intel.com>
2015-03-13 13:03:21 +0800

    Port ISH related mixin configure files to Gmin.
    Port ISH related configure files from Imin to Gmin.
    
    Change-Id: I836a5de3b59cf9699840265e264932423012d0d7
    Signed-off-by: Even Xu <even.xu@intel.com>
    Reviewed-on: https://android.intel.com/341725
    Reviewed-by: Musca, Constantin <constantin.musca@intel.com>
    Reviewed-by: Suman, Viorel <viorel.suman@intel.com>
    Tested-by: Suman, Viorel <viorel.suman@intel.com>

---
+++ groups/sensor-hubs/ish/BoardConfig.mk
@@ -0,0 +1,6 @@
+# enable ish libsensorhub
+ENABLE_SENSOR_HUB_ISH := true
+
+# enable ish sensor HAL
+ENABLE_SENSOR_HAL_ISH := true
+
---
+++ groups/sensor-hubs/ish/product.mk
@@ -0,0 +1,9 @@
+# ISH sensorhubd modules
+PRODUCT_PACKAGES += \
+    sensorhubd      \
+    libsensorhub
+
+# ISH Sensor HAL modules
+PRODUCT_PACKAGES += \
+    sensors.$(TARGET_BOARD_PLATFORM)
+
